13f607ecf11d440f248d2576d6cc82f3fdb872b6
“Nilde <nilde.benedetti@gmail.com>
2026-04-23 16:44:28 +0200

    add: riferimenti dom per comunicare interfaccia utente

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
-console.log('ciao Mamma!');
+
+// Recupera riferimenti al Dom per inout text, bottone e div visualizzazione del messaggio
+
+const chatHistoryEl = document.querySelector('#chat-history');
+const sendMessageFormEl = document.querySelector('#send-message-form');
+const userMessageInputEl = document.querySelector('#user-message');
