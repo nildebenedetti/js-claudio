b46aca040d60a258ce82161e2069d87a791009ef
“Nilde <nilde.benedetti@gmail.com>
2026-04-23 16:16:29 +0200

    first commit

---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+const CLAUDE_API_URL = "https://api.anthropic.com/v1/messages";
+const CLAUDE_MODEL = "claude-sonnet-4-6";
+const CLAUDE_API_KEY = "";
+
+console.log(`ciao ecco qualer modello utilizzo ${CLAUDE_MODEL}`);
