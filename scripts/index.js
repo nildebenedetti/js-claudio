a3f79dedf63fb31e5c92015b0668435b799cdccb
“Nilde <nilde.benedetti@gmail.com>
2026-04-23 16:53:07 +0200

    add: headers della richiesta con spiegazione elementi in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,14 @@
 const chatHistoryEl = document.querySelector('#chat-history');
 const sendMessageFormEl = document.querySelector('#send-message-form');
 const userMessageInputEl = document.querySelector('#user-message');
+
+// creare variabili con parametri necessari per la fetch a Claude
+
+const headers = {
+    "x-api-key": CLAUDE_API_KEY, // mia variabile API key
+    "anthropic-version": "2023-06-01", // la seconda versione, va imparata cosi'
+    "content-type": "application/json", // come vogliamo ricevere la risposta
+    "anthropic-dangerous-direct-browser-access": "true" // consento accesso diretto al browser
+}
+
+
